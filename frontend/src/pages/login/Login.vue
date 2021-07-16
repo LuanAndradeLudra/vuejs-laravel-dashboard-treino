ec2c6b9cdf3fab27654fb5021cfd10fe27280e44
Luan <luanandradeti10@gmail.com>
2021-07-16 15:31:19 -0300

    Login Page Static

--- frontend/src/pages/login/Login.vue
+++ frontend/src/pages/login/Login.vue
@@ -1,47 +1,71 @@
 <template>
-      <div class="login full-size d-flex justify-content-center align-items-center">
+  <div class="login full-size d-flex justify-content-center align-items-center">
+    <div
+      class="content-login d-flex justify-content-center align-items-center flex-column"
+    >
+      <header>
+        <h1>Login</h1>
+      </header>
 
-      <div class="content-login d-flex justify-content-center align-items-center flex-column">
-
-          <header>
-            <h1>Login</h1>
-          </header>
-
-          <section class="form">
-
-            <form method="POST" action="/auth">
-              <div class="mb-3">
-                <label for="exampleInputEmail1" class="form-label">Email</label>
-                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="email@email.com" required>
-                <div id="emailHelp" class="form-text text-gray-medium">Nós nunca compartilharemos seus dados com ninguém.</div>
-              </div>
-              <div class="mb-3">
-                <label for="exampleInputPassword1" class="form-label">Senha</label>
-                <input type="password" class="form-control" id="exampleInputPassword1" minlength="8" placeholder="********" required>
-              </div>
-              <div class="mb-3 form-check">
-                <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                <label class="form-check-label" for="exampleCheck1">Permanecer conectado</label>
-              </div>
-              <div class="d-grid gap-2">
-                <button type="submit" class="btn btn-outline-light btn-block" @click.prevent="login">Login</button>
-              </div>
-            </form>
-
-          </section>
-
-      </div>
-
+      <section class="form">
+        <form method="POST" action="/auth">
+          <div class="mb-3">
+            <label for="exampleInputEmail1" class="form-label">Email</label>
+            <input
+              type="email"
+              class="form-control"
+              id="exampleInputEmail1"
+              aria-describedby="emailHelp"
+              placeholder="email@email.com"
+              required
+            />
+            <div id="emailHelp" class="form-text text-gray-medium">
+              Nós nunca compartilharemos seus dados com ninguém.
+            </div>
+          </div>
+          <div class="mb-3">
+            <label for="exampleInputPassword1" class="form-label">Senha</label>
+            <input
+              type="password"
+              class="form-control"
+              id="exampleInputPassword1"
+              minlength="8"
+              placeholder="********"
+              required
+            />
+          </div>
+          <div class="mb-3 form-check">
+            <input
+              type="checkbox"
+              class="form-check-input"
+              id="exampleCheck1"
+            />
+            <label class="form-check-label" for="exampleCheck1"
+              >Permanecer conectado</label
+            >
+          </div>
+          <div class="d-grid gap-2">
+            <button
+              type="submit"
+              class="btn btn-outline-light btn-block"
+              @click.prevent="login"
+            >
+              Login
+            </button>
+          </div>
+        </form>
+      </section>
     </div>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'Login',
+  name: "Login",
   methods: {
-    login: function () {
-      this.$router.push({name: 'home'});
-    }
+    login: function() {
+      this.$router.push({ name: "home" });
+    },
   },
 };
 </script>
